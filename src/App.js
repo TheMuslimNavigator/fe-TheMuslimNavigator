810343fa8b573d99d4c366f05ca26673201ba607
Marwan Mashaly <mmash102@uottawa.ca>
2023-05-19 22:16:25 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/homePage";
+import EventsPage from "./pages/eventsPage";
+import PrayersPage from "./pages/prayerPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/events" element={<EventsPage />} />
+          <Route path="/prayerTimes" element={<PrayersPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/common/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Events">About</Link>
+          </li>
+          <li>
+            <Link to="/PrayersTime">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
